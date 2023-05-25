9ce5776f897e4d91afa229a9674e706cafefa5ca
Pedro Piveta Barrotti <ppivetabarrotti208@gmail.com>
2023-05-25 02:24:09 -0300

    botao de deletar deslocamento, botao completar

--- src/pages/Deslocamentos.jsx
+++ src/pages/Deslocamentos.jsx
@@ -1,8 +1,9 @@
 import CreateDeslocamento from "../components/CreateDeslocamento";
 import useFetchGet from "../hooks/useFetchGet";
 import { useState, useEffect } from "react";
-import { Plus, Trash } from "phosphor-react";
+import { Plus, Trash, Check } from "phosphor-react";
 import * as Dialog from '@radix-ui/react-dialog';
+import * as AlertDialog from '@radix-ui/react-alert-dialog';
 import axios from "axios";
 
 const Deslocamentos = () => {
@@ -14,7 +15,7 @@ const Deslocamentos = () => {
     const [createHandler, setCreateHandler] = useState(true);
     const [missing, setMissing] = useState('Para criar um deslocamento é necessário ter pelo menos um condutor, um veículo e um cliente cadastrados.');
 
-    let  color = '#6c68ff';
+    let color = '#6c68ff';
 
 
     useEffect(() => {
@@ -61,24 +62,70 @@ const Deslocamentos = () => {
     }, [data]);
 
     const handleDelete = async (id) => {
-        if (data) {
-            try {
-                axios.delete(
-                    `https://api-deslocamento.herokuapp.com/api/v1/Deslocamento/${id}`,
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                        }, data: { id }
-                    }
-                );
-                alert("Deslocamento excluído com sucesso!");
-                window.location.reload();
-            }
-            catch {
-                alert("Não foi possível excluir o deslocamento!");
+        if (formattedData) {
+            const deslocamentoToDelete = formattedData.find((item) => item.id === id);
+            if (deslocamentoToDelete) {
+                const deleteData = {
+                    id: deslocamentoToDelete.id,
+                };
+
+                try {
+                    await axios.delete(
+                        `https://api-deslocamento.herokuapp.com/api/v1/Deslocamento/${id}`,
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            data: deleteData,
+                        }
+                    );
+                    alert("Deslocamento excluído com sucesso!");
+                    window.location.reload();
+                } catch {
+                    alert("Não foi possível excluir o deslocamento!");
+                }
             }
         }
     };
+
+    const handleEncerrar = async (id) => {
+        if (formattedData) {
+            const deslocamentoToEncerrar = formattedData.find((item) => item.id === id);
+            const fimDeslocamento = new Date().toISOString();
+            if (deslocamentoToEncerrar) {
+                const encerrarData = {
+                    id: deslocamentoToEncerrar.id,
+                    kmFinal: 500,
+                    fimDeslocamento: fimDeslocamento,
+                    observacao: 'teste',
+                };
+
+                if (data.inicioDeslocamento > fimDeslocamento) {
+
+                    try {
+                        await axios.put(
+                            `https://api-deslocamento.herokuapp.com/api/v1/Deslocamento/${id}/EncerrarDeslocamento`,
+                            {
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                                data: encerrarData,
+                            }
+                        );
+                        alert("Deslocamento encerrado com sucesso!");
+                        window.location.reload();
+                    } catch (error) {
+                        alert("Não foi possível encerrar o deslocamento!");
+                        console.log(encerrarData)
+                        console.error(error);
+                    }
+                } else {
+                    alert("Não é possível encerrar um deslocamento com data de início maior que a data de fim!");
+                }
+            }
+        }
+    };
+
 
     return (
         <main>
@@ -99,11 +146,44 @@ const Deslocamentos = () => {
             {isPending && <div>Carregando...</div>}
             {error && <div>{error}</div>}
             {formattedData && formattedData.map((deslocamento, index) => (
-                <div className='card' key={index}>
-                    {/* <div>Km inicial: {deslocamento.kmInicial}</div> */}
-                    <div>Início do deslocamento: {deslocamento.inicioDeslocamento}</div>
-                    {deslocamento.fimDeslocamento && <div>Fim do deslocamento: {deslocamento.fimDeslocamento}</div>}
-                    <Trash size={24} color={color} className="delete" onClick={(e) => handleDelete(deslocamento.id)} />
+                <div className='card deslocamento' key={index}>
+                    <div className="info">
+                        <div>Início do deslocamento: {deslocamento.inicioDeslocamento}</div>
+                        {deslocamento.fimDeslocamento && <div>Fim do deslocamento: {deslocamento.fimDeslocamento}</div>}
+                        <div>Km inicial: {deslocamento.kmInicial}</div>
+                        {deslocamento.kmFinal && <div>Km final: {deslocamento.kmFinal}</div>}
+                        <div>Checklist: {deslocamento.checkList}</div>
+                        <div>Motivo: {deslocamento.motivo}</div>
+                        <div>Observação: {deslocamento.observacao}</div>
+                        {/* <div>Condutor: {condutor.data.nome}</div> */}
+                    </div>
+                    <div className="deslocamento-btn">
+                        <button onClick={() => handleEncerrar(deslocamento.id)} disabled={deslocamento.fimDeslocamento ? true : false}>
+                            <Check size={24} color={color} className={deslocamento.fimDeslocamento ? 'encerrado' : ''} />
+                        </button>
+                        {formattedData && (<AlertDialog.Root>
+                            <AlertDialog.Trigger className="dialog-trigger">
+                                <Trash size={24} color={color} className="delete" />
+                            </AlertDialog.Trigger>
+                            <AlertDialog.Portal>
+                                <AlertDialog.Overlay className="dialog-overlay" />
+                                <AlertDialog.Content className="dialog-content">
+                                    <AlertDialog.Title className="dialog-title">Você tem certeza?</AlertDialog.Title>
+                                    <AlertDialog.Description className="dialog-description">
+                                        Você está prestes a excluir o deslocamento {deslocamento.id}. Essa ação não pode ser desfeita.
+                                    </AlertDialog.Description>
+                                    <div className="alert-buttons">
+                                        <AlertDialog.Cancel asChild>
+                                            <button className="Button mauve">Cancelar</button>
+                                        </AlertDialog.Cancel>
+                                        <AlertDialog.Action asChild>
+                                            <button id="delete-btn" onClick={() => handleDelete(deslocamento.id)}>Tudo bem, apagar deslocamento</button>
+                                        </AlertDialog.Action>
+                                    </div>
+                                </AlertDialog.Content>
+                            </AlertDialog.Portal>
+                        </AlertDialog.Root>)}
+                    </div>
                 </div>
             ))}
         </main>
